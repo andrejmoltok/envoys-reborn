b13fe9ef067b74fcb5651af449f8857d2b527a19
Pál András Csokán <csokan.andras87@protonmail.ch>
2023-11-29 15:53:55 +0100

    Update page.tsx

--- src/app/forums_ic/page.tsx
+++ src/app/forums_ic/page.tsx
@@ -4,26 +4,11 @@ import React from 'react';
 import Pole from '@/components/Pole';
 import styles from '@/styles/Layout.module.css';
 import fill from '@/styles/Fill.module.css';
-import { useConvexAuth } from 'convex/react';
 
 export default function IC() {
 
-    const { isAuthenticated } = useConvexAuth();
-
     return (
         <>
-            {(isAuthenticated) &&
-                <>
-                    <div className={styles.border}>
-                        <div className={fill.fill}>
-                            Profile & Buttons
-                        </div>
-                    </div>
-
-                    <Pole />
-                </>
-            }
-
             <div className={styles.border}>
                 <div className={fill.fill}>
                     IC Forums
