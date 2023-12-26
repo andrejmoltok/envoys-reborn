450ccf8c07d2e729906da6b77bf2289ad5210d44
root <root@Arskeliss>
2023-12-26 20:31:15 +0100

    Removed residue dependencies

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,11 @@
 'use client'
 
 import React from 'react';
-import { useUser } from "@clerk/nextjs";
 
 export default function Profile() {
 
-    const { isLoaded, user } = useUser();
-
     return (
         <>
-            Character name: {user?.fullName}
         </>
     )
 }
