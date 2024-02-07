912e069b37cda160fc3a6678add8fa09abd5f7e6
andrejmoltok <csokan.andras87@protonmail.ch>
2024-02-07 06:56:57 +0100

    Add Zustand, add AuthActions and Routes, updated Nav comp

---
+++ src/lib/signin/getUserID.ts
@@ -0,0 +1,19 @@
+'use server'
+
+import { PrismaClient } from '@prisma/client';
+import { loginAuthType } from './loginAuthType';
+
+export default async function GetUserID(data:loginAuthType): Promise<string> {
+    
+    const prisma = new PrismaClient();
+
+    const findUser = await prisma.user.findFirst({
+        where: {
+            username: data.username
+        }
+    });
+
+    await prisma.$disconnect();
+
+    return findUser?.id as string
+}
